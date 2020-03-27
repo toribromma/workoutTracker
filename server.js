3a5a53047f31b59d0f8704605c634c934606b68e
Tori <torimpham@gmail.com>
2020-03-27 13:10:15 -0700

    setting up heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require("express");
 const mongoose = require('mongoose');
 const logger = require("morgan");
+
 
 
 const PORT = process.env.PORT || 3005;
